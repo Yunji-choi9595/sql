194a82c8c2ff8e87f11cf933e643eb17e1d8ce54
DESKTOP-VP29LLL\PC-09 <yunji3424@gmail.com>
2021-03-25 13:09:56 +0900

    no message

--- 최윤지.sql
+++ 최윤지.sql
@@ -1,6 +1,8 @@
 -- outerjoin5 : 과제 
 -- 4에 고객(customer) 이름 컬럼 추가하기 
-SELECT p.*, :cid, cu.cnm, NVL(c.day, 0) day, NVL(c.cnt, 0) cnt 
-FROM cycle c, product p, customer cu
+SELECT *
+FROM
+(SELECT p.*, :cid cid, NVL(c.day, 0) day, NVL(c.cnt, 0) cnt 
+FROM cycle c, product p
 WHERE p.pid = c.pid(+) AND 
-c.cid(+) = :cid;
+c.cid(+) = :cid) a, customer cu;
